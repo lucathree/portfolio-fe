8d2a1525e2f17432a2b1e7727de6230438570706
Changmin Lucas Lee <clee0627@gmail.com>
2025-10-24 23:54:06 +0900

    feat: next.js 기반 초기환경 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+        // Auto-generated Next.js types
+        "types/**",
+    ]),
+    // Disable ESLint formatting rules that conflict with Prettier
+    prettierConfig,
+]);
+
+export default eslintConfig;
